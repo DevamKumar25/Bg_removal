05ee29f24feb156730e0c83684cbcd34831684b4
Devam kumar <devamkumar78@gmail.con>
2025-07-08 13:48:47 +0530

    link import

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext.jsx";
+import { Link } from "react-router-dom";
 
 const Result = () => {
   const { resultImage, image } = useContext(AppContext);
